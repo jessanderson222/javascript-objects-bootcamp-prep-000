9fa79730f0092c7b33e214c8f05cd442391a7100
JESSIE ANDERSON <jessanderson222@gmail.com>
2018-09-07 22:55:22 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,8 @@
+var playlist = { beatles: "hello" };
+
+function updatePlaylist(playlist, artistName, songTitle) {
+   return Object.assign({}, obj, { [key]: value })
+}
+updatePlaylist(playlist, {['Slowdive']: 'sugar'})
+updatePlaylist(playlist, {['My Bloody Valentine']: 'dunno'})
+updatePlaylist(playlist, {['Phil Ochs']: 'dunno lol'})
